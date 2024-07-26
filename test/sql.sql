9a76fe601893be401a522944cfaa1df54996cde2
Alfred <alfred101525@gmail.com>
2024-07-26 22:19:42 +0300

    o

--- test/sql.sql
+++ test/sql.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT name FROM  Regions
-SELECT (season, edible) FROM  Mushrooms WHERE mushroom_id = (Трубчатые
+SELECT (season, edible) FROM  Mushrooms WHERE mushroom_id = (Трубчатые)
 SELECT name FROM  Categories order by desc
 SELECT (name, description) FROM Mushrooms WHERE category_id = (сьедобные) and primary_region_id = (самые большие по размеру регионы)
 SELECT name from Mushrooms where season (весна) and mushroom_id = («Пластинчатые») and size < 6000\
